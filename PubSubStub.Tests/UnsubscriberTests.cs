0f6b2a75091b9d4ce00c819f25bc5f171392a216
Kieranties <Kieran@Kieranties.com>
2013-03-28 22:39:25 +0000

    Unsubscriber unit tests

--- PubSubStub.Tests/UnsubscriberTests.cs
+++ PubSubStub.Tests/UnsubscriberTests.cs
@@ -1,11 +1,77 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using NUnit.Framework;
+using System.Collections.ObjectModel;
 
 namespace PubSubStub.Tests
 {
-    class UnsubscriberTests
+    [TestFixture]
+    public class UnsubscriberTests
     {
+
+        #region Constructor
+        [Test]
+        public void Constructor_WithNullValues_ReturnsUnsubscriberInstance()
+        {
+            var nullInstance = new Unsubscriber<object>(null, new Collection<object>());
+            var nullCollection = new Unsubscriber<object>(new object(), null);
+            var nullInstanceAndCollection = new Unsubscriber<object>(null, null);
+
+            Assert.IsNotNull(nullInstance);
+            Assert.IsNotNull(nullCollection);
+            Assert.IsNotNull(nullInstanceAndCollection);
+        }
+
+        [Test]
+        public void Constructor_WithValidValues_ReturnsUnsubscriberInstance()
+        {
+            var obj = new object();
+            var collection = new Collection<object> {obj};
+            
+            var unsubscriber = new Unsubscriber<object>(obj, collection);
+            
+            Assert.IsNotNull(unsubscriber);
+        }
+
+        [Test]
+        public void Constructor_WithCollectionMissingInstance_ReturnsUnsubscriberInstance()
+        {
+            var unsubscriber = new Unsubscriber<object>(new object(), new Collection<object>());
+
+            Assert.IsNotNull(unsubscriber);
+        }
+        #endregion
+
+        #region Dispose
+        [Test]
+        public void Dispose_WithNullConstructorParameters_DoesNotThrow()
+        {
+            var nullInstance = new Unsubscriber<object>(null, new Collection<object>());
+            var nullCollection = new Unsubscriber<object>(new object(), null);
+            var nullInstanceAndCollection = new Unsubscriber<object>(null, null);
+
+            Assert.DoesNotThrow(nullInstance.Dispose);
+            Assert.DoesNotThrow(nullCollection.Dispose);
+            Assert.DoesNotThrow(nullInstanceAndCollection.Dispose);
+        }
+
+        [Test]
+        public void Dispose_WithCollectionMissingInstance_DoesNotThrow()
+        {
+            var unsubscriber = new Unsubscriber<object>(new object(), new Collection<object>());
+
+            Assert.DoesNotThrow(unsubscriber.Dispose);
+        }
+
+        [Test]
+        public void Dispose_WithCollectionContainingInstance_RemovesInstanceFromCollection()
+        {
+            var obj = new object();
+            var collection = new Collection<object> {obj};
+
+            var unsubscriber = new Unsubscriber<object>(obj, collection);
+            unsubscriber.Dispose();
+
+            Assert.IsEmpty(collection);
+        }
+        #endregion
     }
 }
