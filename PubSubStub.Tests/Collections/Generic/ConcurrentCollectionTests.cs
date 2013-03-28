f7918c72e55fe5a1a0287d3d093591a465cb8d80
Kieranties <Kieran@Kieranties.com>
2013-03-28 09:57:25 +0000

    Stubbed test classes

---
+++ PubSubStub.Tests/Collections/Generic/ConcurrentCollectionTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PubSubStub.Tests.Collections.Generic
+{
+    class ConcurrentCollectionTests
+    {
+    }
+}
---
+++ PubSubStub.Tests/PublisherFactoryTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PubSubStub.Tests
+{
+    class PublisherFactoryTests
+    {
+    }
+}
